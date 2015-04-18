f68725f240833c96ad906be1217b0185b0b43995
nKast <nkastellanos@gmail.com>
2015-04-18 05:39:32 +0300

    Add Line3.cs & add Plane Extensions

---
+++ Source/Maths/Extensions.cs
@@ -0,0 +1,47 @@
+﻿#region License 
+//   Copyright 2015 Kastellanos Nikolaos
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#endregion
+
+using Microsoft.Xna.Framework;
+
+namespace tainicom.Aether.Maths
+{
+    public static class Extensions
+    {       
+        public static float Distance(this Plane plane, ref Vector3 point)
+        {
+            float dot = Vector3.Dot(plane.Normal, point);
+            float value = dot - plane.D;
+            value = value / Vector3.Dot(plane.Normal, plane.Normal); // for plane normals that are not normalized
+            return value;
+        }
+
+        public static bool Intersects(this Plane planeA, ref Plane planeB, out Line3 line)
+        {
+            line = new Line3(Vector3.Zero, Vector3.Zero);
+            Vector3.Cross(ref planeA.Normal, ref planeB.Normal, out line.Direction);
+            
+            float demon = Vector3.Dot(line.Direction, line.Direction);
+            if(demon < float.Epsilon) return false; // parallel?
+
+            // point on intersection line
+            line.Position = Vector3.Cross(planeA.D * planeB.Normal - planeB.D * planeA.Normal, line.Direction);
+            line.Position = line.Position / demon; // for plane normals that are not normalized
+            
+            return true;
+        }
+            
+    }
+}
---
+++ Source/Maths/Line3.cs
@@ -0,0 +1,65 @@
+﻿#region License 
+//   Copyright 2015 Kastellanos Nikolaos
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#endregion
+
+using Microsoft.Xna.Framework;
+
+namespace tainicom.Aether.Maths
+{
+    public struct Line3
+    {
+        public Vector3 Direction;
+        public Vector3 Position;
+        
+        public Line3(Vector3 position, Vector3 direction)
+        {
+            this.Position = position;
+            this.Direction = direction;
+        }
+
+        public static void ClosestPoints(ref Line3 L1, ref Line3 L2, out Vector3 Q1, out Vector3 Q2)
+        {
+            Vector3 P1 = L1.Position;
+            Vector3 P2 = L2.Position;
+            Vector3 d1 = L1.Direction;
+            Vector3 d2 = L2.Direction;
+
+            var r = P1 - P2;
+
+            var a = Vector3.Dot(d1, d1);
+            var b = Vector3.Dot(d1, d2);
+            var c = Vector3.Dot(d1, r);
+            var e = Vector3.Dot(d2, d2);
+            var f = Vector3.Dot(d2, r);
+
+
+            var d = a*e - b*b;
+            if (d == 0)
+            {
+                // parallel lines
+                //var s = 0;
+                //var t = ??
+            }
+
+            var s = (b*f - c*e)/d;
+            var t = (a*f - b*c)/d;
+
+            Q1 = P1 + s * d1;
+            Q2 = P2 + t * d2;
+            return;
+        }
+    }
+
+}
