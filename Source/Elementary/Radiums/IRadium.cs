218f5cd810ff869a721672852af13296e66cef6f
nKast <nkastellanos@gmail.com>
2015-07-19 17:29:41 +0300

    Add IRadium

---
+++ Source/Elementary/Radiums/IRadium.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tainicom.Aether.Elementary.Radiums
+{
+    public interface IRadium<T>:IAether where T: struct
+    {
+        T Value { get; set; }
+    }
+}
