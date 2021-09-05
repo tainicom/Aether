a008b743de9c191b495b4c04c70a72ab5b3b5199
Nick Kastellanos <nkastellanos@gmail.com>
2021-09-05 12:59:20 +0300

    SpatialBase

---
+++ Source/Core/Spatial/SpatialBase.cs
@@ -0,0 +1,83 @@
+﻿#region License
+//   Copyright 2015 Kastellanos Nikolaos
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+#endregion
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using tainicom.Aether.Elementary;
+using tainicom.Aether.Elementary.Serialization;
+using tainicom.Aether.Elementary.Spatial;
+
+namespace tainicom.Aether.Core.Spatial
+{
+    public class SpatialBase : ISpatial,
+        IAetherSerialization, IAether
+    {
+        protected Vector3 _scale = Vector3.One;
+        protected Quaternion _rotation = Quaternion.Identity;
+        protected Vector3 _position;
+
+        protected Matrix _localTransform = Matrix.Identity;
+
+        #region Implement ISpatial Properties
+
+        public Vector3 Position
+        {
+            get { return _position; }
+            set { _position = value; UpdateLocalTransform(); }
+        }
+
+        public Quaternion Rotation
+        {
+            get { return _rotation; }
+            set { _rotation = value; UpdateLocalTransform(); }
+        }
+
+        public Vector3 Scale
+        {
+            get { return _scale; }
+            set { _scale = value; UpdateLocalTransform(); }
+        }
+
+        public Matrix LocalTransform { get { return _localTransform; } }
+
+        #endregion ISpatial Properties
+
+        protected virtual void UpdateLocalTransform()
+        {
+            _localTransform = Matrix.CreateScale(_scale)
+                            * Matrix.CreateFromQuaternion(_rotation)
+                            * Matrix.CreateTranslation(_position);
+        }
+
+
+        #region Implement IAetherSerialization
+        public void Save(IAetherWriter writer)
+        {
+            writer.WriteVector3("Position", _position);
+            writer.WriteVector3("Scale", _scale);
+            writer.WriteQuaternion("Rotation", _rotation);
+        }
+        public void Load(IAetherReader reader)
+        {
+            reader.ReadVector3("Position", out _position);
+            reader.ReadVector3("Scale", out _scale);
+            reader.ReadQuaternion("Rotation", out _rotation);
+            UpdateLocalTransform();
+        }
+        #endregion
+    }
+}
